74c2172cbe142230ebdd35c3d582315e631018c4
Anmol Singh <Anmol_singh@live.no>
2022-07-31 13:33:23 +0200

    Add empty img description in ProjectDisplay

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -10,7 +10,7 @@ function ProjectDisplay() {
     return (
         <div className="project">
             <h1> {project.name} </h1>
-            <img src={project.image} />
+            <img src={project.image} alt="" />
             <p>
                 <b>Skills: </b> {project.skills}
             </p>
